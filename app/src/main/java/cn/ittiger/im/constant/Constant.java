7e0f340312a8f71f110e894676048dd70ecdb5bf
wangdsh <603204647@qq.com>
2017-03-27 21:09:32 +0800

    add blank line

--- app/src/main/java/cn/ittiger/im/constant/Constant.java
+++ app/src/main/java/cn/ittiger/im/constant/Constant.java
@@ -9,4 +9,5 @@ public class Constant {
     public static final String SERVER_IP = "192.168.1.157";
     public static final String SERVER_NAME = "192.168.1.157";
     public static final int SERVER_PORT = 5222;
+
 }
