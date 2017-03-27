0d1757e1f7e34cd70f58af2e122909c6b287f9f7
wangdsh <603204647@qq.com>
2017-03-27 21:05:41 +0800

    add constants

--- app/src/main/java/cn/ittiger/im/constant/Constant.java
+++ app/src/main/java/cn/ittiger/im/constant/Constant.java
@@ -6,4 +6,7 @@ package cn.ittiger.im.constant;
 public class Constant {
 
     public static final String MULTI_CHAT_ADDRESS_SPLIT = "@conference.";
+    public static final String SERVER_IP = "192.168.1.157";
+    public static final String SERVER_NAME = "192.168.1.157";
+    public static final int SERVER_PORT = 5222;
 }
