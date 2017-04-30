063797010da5dcf93c40ef63375134de7f077cfd
wangdsh <603204647@qq.com>
2017-04-30 16:32:13 +0800

    replace tab to spaces

--- app/src/main/java/cn/mobcommu/zim/activity/AccountMngActivity.java
+++ app/src/main/java/cn/mobcommu/zim/activity/AccountMngActivity.java
@@ -18,66 +18,66 @@ import cn.mobcommu.util.UIUtil;
  * 账号管理
  */
 public class AccountMngActivity extends BaseActivity {
-	/**
-	 * 注销登陆
-	 */
-	@BindView(R.id.btn_logout)
-	Button mBtnLogout;
-	/**
-	 * 用户状态修改
-	 */
-	@BindView(R.id.rg_user_state)
-	RadioGroup mUserState;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_account_mng_layout);
+    /**
+     * 注销登陆
+     */
+    @BindView(R.id.btn_logout)
+    Button mBtnLogout;
+    /**
+     * 用户状态修改
+     */
+    @BindView(R.id.rg_user_state)
+    RadioGroup mUserState;
 
-		mUserState.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			@Override
-			public void onCheckedChanged(RadioGroup group, int checkedId) {
-				switch(checkedId) {
-					case R.id.rb_user_online://在线
-						changeState(0);
-						break;
-					case R.id.rb_user_busy://忙碌
-						changeState(2);
-						break;
-					case R.id.rb_disconnect://断开连接
-						disconnect();
-						break;
-				}
-			}
-		});
-	}
-	
-	public void changeState(int code) {
-		if(SmackManager.getInstance().updateUserState(code)) {
-			UIUtil.showToast(this, "修改状态成功");
-		} else {
-			UIUtil.showToast(this, "修改状态失败");
-		}
-	}
-	
-	public void disconnect() {
-		if(SmackManager.getInstance().disconnect()) {
-			ActivityUtil.finishActivity(this);
-		} else {
-			UIUtil.showToast(this, "断开连接失败");
-		}
-	}
-	
-	/**
-	 * 注销登陆
-	 * @param v
-	 */
-	@OnClick(R.id.btn_logout)
-	public void onLogoutClick(View v) {
-		if(SmackManager.getInstance().logout()) {
-			ActivityUtil.finishActivity(this);
-		} else {
-			UIUtil.showToast(this, "注销失败");
-		}
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_mng_layout);
+
+        mUserState.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId) {
+                    case R.id.rb_user_online://在线
+                        changeState(0);
+                        break;
+                    case R.id.rb_user_busy://忙碌
+                        changeState(2);
+                        break;
+                    case R.id.rb_disconnect://断开连接
+                        disconnect();
+                        break;
+                }
+            }
+        });
+    }
+
+    public void changeState(int code) {
+        if(SmackManager.getInstance().updateUserState(code)) {
+            UIUtil.showToast(this, "修改状态成功");
+        } else {
+            UIUtil.showToast(this, "修改状态失败");
+        }
+    }
+
+    public void disconnect() {
+        if(SmackManager.getInstance().disconnect()) {
+            ActivityUtil.finishActivity(this);
+        } else {
+            UIUtil.showToast(this, "断开连接失败");
+        }
+    }
+
+    /**
+     * 注销登陆
+     * @param v
+     */
+    @OnClick(R.id.btn_logout)
+    public void onLogoutClick(View v) {
+        if(SmackManager.getInstance().logout()) {
+            ActivityUtil.finishActivity(this);
+        } else {
+            UIUtil.showToast(this, "注销失败");
+        }
+    }
 }
