56f1da9721b676cfeb35ee9a3017f6859fd60833
wangdsh <603204647@qq.com>
2017-05-04 19:03:18 +0800

    Bitmap too large to be uploaded into a text

--- app/src/main/java/cn/mobcommu/zim/activity/EnlargeImage.java
+++ app/src/main/java/cn/mobcommu/zim/activity/EnlargeImage.java
@@ -3,8 +3,10 @@ package cn.mobcommu.zim.activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 
 import com.bm.library.PhotoView;
 
@@ -22,8 +24,20 @@ public class EnlargeImage extends AppCompatActivity {
         String image_url = intent.getStringExtra("image_url");
         Bitmap bm = BitmapFactory.decodeFile(image_url);
 
+        // 处理过大图片 "Bitmap too large to be uploaded into a texture"
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+
+        // 缩放
+        float scaleHt =(float) width/bm.getWidth();
+        Bitmap scaled = Bitmap.createScaledBitmap(bm, width,
+                (int)(bm.getWidth()*scaleHt), true);
+
+
         PhotoView photoView = (PhotoView) findViewById(cn.mobcommu.zim.R.id.photoview_id);
-        photoView.setImageBitmap(bm);
+        photoView.setImageBitmap(scaled);
         photoView.enable(); // 开启放缩功能
     }
 }
